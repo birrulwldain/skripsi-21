f43b0f6f3ceac1801fbe6364d63675ccc614cea1
Birrul Walidain <birrulwldain@gmail.com>
2025-03-23 00:56:19 +0700

    reset3
    reset all branch

--- project/preprocessing/normalize.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-
-def minmax_normalize(spectrum):
-    return (spectrum - np.min(spectrum)) / (np.max(spectrum) - np.min(spectrum))
-
-def zscore_normalize(spectrum):
-    return (spectrum - np.mean(spectrum)) / np.std(spectrum)
