e3698a38139cacc0fa00d3257a6544f4c43c65c7
Birrul Walidain <birrulwldain@gmail.com>
2025-03-23 01:21:43 +0700

    Restructure Project Thesis and Prototype 2

--- thesis/build.bat
+++ thesis/build.bat
@@ -1,16 +1,16 @@
 pdflatex -interaction=nonstopmode --shell-escape seminar-proposal.tex
-bibtex seminar-proposal.aux
+biber seminar-proposal.aux
 pdflatex -interaction=nonstopmode --shell-escape seminar-proposal.tex
 pdflatex -interaction=nonstopmode --shell-escape seminar-proposal.tex
 
 @REM Uncommnet bagian ini jika inigin mengkompile file seminar-hasil
 @REM pdflatex -interaction=nonstopmode --shell-escape seminar-hasil.tex
-@REM bibtex seminar-hasil.aux 
+@REM biber seminar-hasil.aux 
 @REM pdflatex -interaction=nonstopmode --shell-escape seminar-hasil.tex
 @REM pdflatex -interaction=nonstopmode --shell-escape seminar-hasil.tex
 
 @REM Uncomment bagian ini jika ingin mencompile file skripsi
 @REM pdflatex -interaction=nonstopmode --shell-escape skripsi.tex
-@REM bibtex skripsi.aux 
+@REM biber skripsi.aux 
 @REM pdflatex -interaction=nonstopmode --shell-escape skripsi.tex
 @REM pdflatex -interaction=nonstopmode --shell-escape skripsi.tex
